e80f62a3437456a54f68ea6a3e53ba02782b8199
codingsloth283 <f3139865@gmail.com>
2025-07-22 20:24:57 +0100

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,5 +1,9 @@
 def Main():
-  print("Hello World")
+  select = input("type 1 for hello world and 2 for exit")
+  if select == "1":
+    print("Hello World")
+  if select == "2":
+    exit(2)
 def Run():
   Main()
 
