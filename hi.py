b89f1fb20afd5b3833dca4ad1bebd8d18adb1d76
codingsloth283 <f3139865@gmail.com>
2025-07-22 20:32:30 +0100

    hi.py

--- hi.py
+++ hi.py
@@ -1,5 +1,5 @@
 def Main():
-  select = input("type 1 for hello world and 2 for exit")
+  select = input("type 1 for hello world and 2 for exit: ")
   if select == "1":
     print("Hello World")
   if select == "2":
