66d70facd15b83b229d4711989a41c15da818f82
codingsloth283 <f3139865@gmail.com>
2025-07-22 20:41:30 +0100

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,11 +1,6 @@
-def Main():
-  select = input("type 1 for hello world and 2 for exit: ")
-  if select == "1":
-    print("Hello World")
-  if select == "2":
-    exit(2)
-def Run(): 
-  Main()
-
-Run()
-
+#notes
+print("This is a notes app")
+inputnote = input("input a note")
+title_input = input("input a title")
+print(title_input)
+print(inputnote)
