f1f890748c841bbc201798551323ee6fa455b493
codingsloth283 <f3139865@gmail.com>
2025-07-23 15:55:51 +0100

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -1,12 +1,24 @@
 
 #notes
 print("This is a notes app, the notes will be saved in a txt file in the Directory you are in")
-inputnote = input("input a note: ")
-title_input = input("input a title: ")
-print("Title: "+title_input)
-print(inputnote)
+inputyorn = input("type y to input a note and n to not")
+if inputyorn == "y":
+	
+	inputnote = input("input a note: ")
+	title_input = input("input a title: ")
+	print("Title: "+title_input)
+	print("Note: "+inputnote)
 
-with open("notes.txt", "w") as f:
-    f.write("Title: "+title_input\n)
-    f.write(inputnote\n)
+	with open("notes.txt", "a") as f:
+		f.write("Title: "+title_input+'\n')
+		f.write("Note: "+inputnote+'\n')
+
+print("previous notes")
+with open("notes.txt") as inp:
+	data = set(inp.read().splitlines())
+	print(data)
+
+inputclear = input("type clear to clear the notes.txt file")
+if inputclear == "clear":
+	open('notes.txt', 'w').close()
 
