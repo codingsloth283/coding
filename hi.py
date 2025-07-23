447e386215668b529a041d04a239cd5188655156
codingsloth283 <f3139865@gmail.com>
2025-07-23 12:36:36 +0100

    add noites to a file

--- hi.py
+++ hi.py
@@ -1,6 +1,12 @@
+
 #notes
-print("This is a notes app")
+print("This is a notes app, the notes will be saved in a txt file in the Directory you are in")
 inputnote = input("input a note: ")
 title_input = input("input a title: ")
 print("Title: "+title_input)
 print(inputnote)
+
+with open("notes.txt", "w") as f:
+    f.write("Title: "+title_input)
+    f.write(inputnote)
+
